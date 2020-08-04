1faa0e394c0e1e83ddbe6b90a72fc8939b14a417
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-08-03 17:11:15 -0700

    remove test upload - we know it works

--- backend/apiTest.php
+++ backend/apiTest.php
@@ -8,11 +8,4 @@ require 'GoogleDriveUtils.php';
 
 $util = new GoogleDriveUtils();
 $util->getFilesInDrive();
-
-echo "Testing File Upload...";
-$util->uploadFiles("testDocs/jpg_test.jpg");
-$util->uploadFiles("testDocs/png_test.png");
-$util->uploadFiles("testDocs/pdf_test.pdf");
-$util->uploadFiles("testDocs/docx_test.docx");
-echo "\nUploaded File.";
 ?>
