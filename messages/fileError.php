1e24f92d19f252cf11c0261b476dc6bea0266716
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-08-08 11:14:35 -0700

    Add error message files

---
+++ messages/fileError.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <link rel="stylesheet" href="../messages/fileError.css">
+    <script src="https://kit.fontawesome.com/81f93d9156.js" crossorigin="anonymous"></script>
+
+    <?php
+        class fileError {
+            private $message;
+
+            public function __construct($fileErrorM){
+                $this->message = $fileErrorM;
+            }
+
+            public function printMessage(){
+                  echo '<div id="filemsg" class="fileErrorContainer">
+                            <div class="fileErrorMessage">
+                                <p class="fileErrorTitle">
+                                    <i class="fas fa-check fa-md fa-fw" aria-hidden="true" style="color:#b31515;"></i>';
+                                    echo ' - ' . $this->message;
+                          echo '</p>
+                            </div>
+                        </div>';
+            }
+        }
+    ?>
+</html>
