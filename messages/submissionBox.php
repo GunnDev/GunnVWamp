ae239fe4ea23db24d7058f00ecdf7e2807e335a3
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-08-25 15:40:48 -0700

    add id to submissions

--- messages/submissionBox.php
+++ messages/submissionBox.php
@@ -10,20 +10,22 @@
             private $lname;
             private $fileName;
             private $fileLink;
+            private $submissionID;
             
             public function __construct($fname, $lname, $fileName, $fileLink){
                 $this->fname = $fname;
                 $this->lname = $lname;
                 $this->fileName = $fileName;
                 $this->fileLink = $fileLink;
+                $this->submissionID = $fname . '-' . $lname . '-' . $fileName;
             }
 
             public function printMessage(){
-          echo '<div class="submissionBoxContainer">
-                    <section class="submissionBoxSection">
+          echo "<div id='$this->submissionID' class='submissionBoxContainer'>
+                    <section class='submissionBoxSection'>
                         <article>
-                            <p class="showSubmissionInfo">
-                                <span style="color:#636363;">';
+                            <p class='showSubmissionInfo'>
+                                <span style='color:#636363;'>";
                                 echo $this->fname . ' ' . $this->lname;
                         echo "</span>";
                                 echo ' - ' .  $this->fileName;
@@ -34,11 +36,6 @@
                                 <a href='$this->fileLink' target='_blank' class='linkBtn' title='Open Submission?'>
                                     <i class='fas fa-link fa-lg centeredIcon'></i>
                                 </a>
-                            </div>
-                        </article>
-                        <article>
-                            <div class='submissionsBtnDiv'>
-                                
                             </div>
                         </article>
                     </section>
