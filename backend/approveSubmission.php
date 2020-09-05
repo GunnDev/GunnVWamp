33a2f4748b3c75ceb007fa92c5e9470e7bb86c49
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-09-05 16:02:29 -0700

    add file to update submisisons db

--- backend/approveSubmission.php
+++ backend/approveSubmission.php
@@ -8,5 +8,35 @@
     $adminPass = $_POST['passwordApprove'];
     $fileID = $_POST['fileIDApprove'];
 
-    echo $numHours . " " . $adminPass . " " . $fileID;
+    // Get Submisison Entry
+    $getEntrystmt= $mysqli->prepare("SELECT * FROM submissions WHERE id_of_file = ?");
+    $getEntrystmt->bind_param("s", $fileID);
+    $getEntrystmt->execute();
+    $getEntrystmt->store_result();
+    $getEntrystmt->bind_result($subid, $nameOfFile, $id_of_file, $approved, $declined, $usersID);
+
+    // For Verifying Password
+    $getEntrystmt = $mysqli->prepare("SELECT studentpass FROM users WHERE studentid = 1");
+    $getEntrystmt->execute();
+    $getEntrystmt->store_result();
+    $getEntrystmt->bind_result($apass);
+
+    if ($getEntrystmt->num_rows == 1){
+        $getEntrystmt->fetch();
+
+        if (password_verify($adminPass, $apass)){
+            $stmt = $mysqli->prepare("UPDATE submissions SET approved = ? WHERE id_of_file = ?");
+            $stmt->bind_param("is", $numHours, $fileID);
+
+            $stmt->execute();
+            $stmt->close();
+
+            header("Location: ../pages/submissions.php");
+        } else {
+            header("Location: ../pages/submissions.php?review=pass");
+        }
+    } else {
+        header("Location: ../pages/submissions.php?review=failed");
+    }
+    
 ?>
