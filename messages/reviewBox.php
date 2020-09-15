3f7966a47322a16a10584ecaaaec0959ebb26fe9
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-09-14 17:18:40 -0700

    shows if it was approved or declined in reviewed

--- messages/reviewBox.php
+++ messages/reviewBox.php
@@ -11,12 +11,16 @@
             private $lname;
             private $fileName;
             private $fileID;
+            private $approved;
+            private $declined;
 
-            public function __construct($fname, $lname, $fileName, $fileID){
+            public function __construct($fname, $lname, $fileName, $fileID, $approved, $declined){
                 $this->fname = $fname;
                 $this->lname = $lname;
                 $this->fileName = $fileName;
                 $this->fileID = $fileID;
+                $this->approved = $approved;
+                $this->declined = $declined;
             }
 
             public function printMessage(){
@@ -30,12 +34,26 @@
                                 echo ' - ' .  $this->fileName;
                       echo "</p>
                         </article>
+
                         <article>
                             <div onclick='editSubmission(" . "\"" . $this->fileID . "\"" . ")' class='linkBtnDiv'>
                                 <i class='fas fa-pencil-alt fa-lg centeredIcon' style='color:#1a73e8' title='Edit Submission'></i>
                             </div>
-                        </article>
-                    </section>
+                        </article>";
+
+                        if ($this->approved == -1) {
+                            echo "
+                            <article>
+                                <i class='fas fa-times fa-2x centeredIconD' style='color:#e33d3d' title='Declined'></i>
+                            </article>";
+                        } else if ($this->approved > -1) {
+                            echo "
+                            <article>
+                                <i class='fas fa-check fa-2x centeredIconC' style='color:#24a647' title='Declined'></i>
+                            </article>";
+                        }
+
+              echo "</section>
                 </div>";
             }
         }
