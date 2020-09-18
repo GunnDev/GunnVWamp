f1cedf7587fc931c64f6ddd23ab48b3be99cb7ce
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-09-17 22:24:54 -0700

    Update students.php

--- pages/students.php
+++ pages/students.php
@@ -25,6 +25,8 @@ Gunn Volunteering
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/81f93d9156.js" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <meta charset="utf-8">
+        
         <script>
             $(document).ready(function(){
                 $("#searchStudentsField").focus(function() {
@@ -39,7 +41,6 @@ Gunn Volunteering
                 });
             });
         </script>
-        <meta charset="utf-8">
 
         <title>Gunn Volunteering | Students</title>
     </head>
