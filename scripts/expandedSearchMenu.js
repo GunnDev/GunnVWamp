5ea4a6670fa2ade03a3935989c98f89b40ed268d
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2021-01-05 12:43:31 -0800

    Save the user's input

--- scripts/expandedSearchMenu.js
+++ scripts/expandedSearchMenu.js
@@ -23,3 +23,46 @@ function expandedSearchMenu() {
 function removeSearch() {
     window.location.href = "http://localhost/GunnVWamp/pages/students.php?&g1=1&g2=1&g3=1&g4=1&adv=t"; 
 }
+
+window.onload = function() {
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+
+    var sT = url.searchParams.get("st");
+    var g1 = url.searchParams.get("g1");
+    var g2 = url.searchParams.get("g2");
+    var g3 = url.searchParams.get("g3");
+    var g4 = url.searchParams.get("g4");
+
+    if(g1 == 1) {
+        document.getElementById('grade1').checked = true;
+    } else {
+        document.getElementById('grade1').checked = false;
+    }
+
+    if(g2 == 1) {
+        document.getElementById('grade2').checked = true;
+    } else {
+        document.getElementById('grade2').checked = false;
+    }
+
+    if(g3 == 1) {
+        document.getElementById('grade3').checked = true;
+    } else {
+        document.getElementById('grade3').checked = false;
+    }
+
+    if(g4 == 1) {
+        document.getElementById('grade4').checked = true;
+    } else {
+        document.getElementById('grade4').checked = false;
+    }
+
+    console.log(sT);
+
+    if(sT == "F") {
+        document.getElementById('alphabeticalF').checked = true;
+    } else if (sT == "L") {
+        document.getElementById('alphabeticalL').checked = true;
+    }
+}
